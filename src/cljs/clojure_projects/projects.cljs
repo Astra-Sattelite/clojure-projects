88e3ed2f6af549cc0aa4001a6c2828a46860bf43
ydrExty <liaroffear@mail.ru>
2020-12-21 23:08:09 +0500

    FIX: cljss problems

--- src/cljs/clojure_projects/projects.cljs
+++ src/cljs/clojure_projects/projects.cljs
@@ -4,18 +4,18 @@
   {:title  "Counter"
    :route  "/counter/"
    :descr  "Learning how to work with events\\subs"
-   :img "https://static.wikia.nocookie.net/bhlx/images/2/2a/Laffey.png/revision/latest?cb=20200101173956"})
+   :img "https://static.wikia.nocookie.net/bhlx/images/2/2a/Laffey.png"})
 
 (def osu
   {:title "Minimal osu"
    :route "/osu/"
    :descr "Rythm is just click away!"
-   :img "https://static.wikia.nocookie.net/azur-lane/images/9/9a/Kaga.png/revision/latest?cb=20190830172320&path-prefix=ru"})
+   :img "https://i.pinimg.com/originals/62/08/9e/62089ee9672198cd380b938aec5f1577.gif"})
 
 (def azur-farm
   {:title "Tracker"
    :route "/azurfarm/"
    :descr "Tracker for drops in Azur Lane"
-   :img "https://static.wikia.nocookie.net/azur-lane/images/0/06/Akagi.png/revision/latest/top-crop/width/360/height/450?cb=20190829151734&path-prefix=ru"})
+   :img "https://i.pinimg.com/originals/29/00/92/2900920e2ac0a0c8f16eba53c837315b.gif"})
 
 (def projects [counter osu azur-farm])
